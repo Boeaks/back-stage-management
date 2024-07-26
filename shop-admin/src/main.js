4583c9106b58dbe2bf6f44f5a18706615a1a9132
Boeaks <ch_xnzj@163.com>
2024-07-26 16:49:46 +0800

    update element-plus to chinese

--- shop-admin/src/main.js
+++ shop-admin/src/main.js
@@ -6,12 +6,16 @@ import 'element-plus/dist/index.css'
 import store from './store'
 import 'virtual:windi.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
 
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus,{
+  locale: zhCn,
+})
 // 图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
